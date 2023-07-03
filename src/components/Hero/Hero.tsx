6dcb1d092b902e5f8a245d2f809926cf7952a4f4
Johan Sveningsson <johan@devwave.se>
2023-07-03 21:24:49 +0200

    fix

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -24,14 +24,24 @@ const Hero: React.FunctionComponent = () => (
             </h1>
           </div>
           <div className=" lg:mt-[25rem]">
-            <p className="text-red-500 hidden lg:flex max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl">
-              I´m Emma, a &quot; creative and spider in the web &quot; based in
-              Sweden. Before I worked with Social Media I was an influencer, now
-              I am both. I have been in the digital world for over 11 years and
-              still counting. I hold a university degree in digital marketing, I
-              am certified, have led projects, and have served as a marketing
-              manager.
+            <p className="text-red-500 hidden lg:flex max-w-2xl mb-6 font-light lg:mb-2 md:text-lg lg:text-xl">
+              I´m Emma, creative and organized, a star of multitasking, based in
+              Sweden. Before I worked with Social Media for companies, I was an
+              influencer, now I am both. I have been in the digital world for
+              over 11 years and still counting. I hold a university degree in
+              digital marketing, I am certified, have led projects, and have
+              served as a marketing manager.
             </p>
+            <p className="text-red-500 font-light lg:mb-2 md:text-lg lg:text-xl lg:mb-6">
+              Do you need help with gaining visibility on social media? Reach
+              out to me!
+            </p>
+            <a
+              className="border-2 font-bold text-lg transition ease-in-out hover:-translate-y-1 hover:scale-110 border-red-500 py-2 px-3 text-center text-red-500 hover:bg-red-500 hover:text-white"
+              href="mailto:emmafelsing@hotmail.com"
+            >
+              Email me
+            </a>
           </div>
         </div>
 
@@ -44,13 +54,28 @@ const Hero: React.FunctionComponent = () => (
             src="/images/emma_sun_1.jpg"
           />
         </div>
-        <p className="text-red-500 lg:hidden max-w-2xl mb-10 mt-10 font-light lg:mb-8 md:text-lg lg:text-xl">
-          I´m Emma, a &quot; creative and spider in the web &quot; based in
-          Sweden. Before I worked with Social Media I was an influencer, now I
-          am both. I have been in the digital world for over 11 years and still
-          counting. I hold a university degree in digital marketing, I am
-          certified, have led projects, and have served as a marketing manager.
-        </p>
+        <div className="lg:hidden max-w-2xl mb-10 mt-10 lg:mb-8 ">
+          <p className="text-red-500 font-light md:text-lg lg:text-xl">
+            I´m Emma, creative and organized, a star of multitasking, based in
+            Sweden. Before I worked with Social Media for companies, I was an
+            influencer, now I am both. I have been in the digital world for over
+            11 years and still counting. I hold a university degree in digital
+            marketing, I am certified, have led projects, and have served as a
+            marketing manager.
+          </p>
+          <p className="text-red-500 pt-5 font-light md:text-lg lg:text-xl ">
+            Do you need help with gaining visibility on social media? Reach out
+            to me!
+          </p>
+          <div className="mt-8">
+            <a
+              className="border-2 font-bold text-lg transition ease-in-out hover:-translate-y-1 hover:scale-110 border-red-500 py-2 px-3 text-center text-red-500 hover:bg-red-500 hover:text-white"
+              href="mailto:emmafelsing@hotmail.com"
+            >
+              Email me
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </section>
